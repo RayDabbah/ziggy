23630ca958223b20d7080237779c2106c32cd30c
raydabbah <raydabbah@gmail.com>
2019-04-10 17:37:36 -0400

    only put a comma into the view if there is a 2nd arg

--- src/ZiggyServiceProvider.php
+++ src/ZiggyServiceProvider.php
@@ -19,12 +19,12 @@ class ZiggyServiceProvider extends ServiceProvider
         });
 
         $this->app['blade.compiler']->directive('routes', function ($group) {
-           $group =  explode(',', $group);
+           $group =  explode(', ', $group);
 
-            $guard = isset($group[1]) ? trim($group[1]) : false;
+            $guard = isset($group[1]) ? ',' . trim($group[1]) : false;
             $group = trim($group[0]);
 
-            return "<?php echo app('" . BladeRouteGenerator::class . "')->generate({$group}, {$guard}); ?>";
+            return "<?php echo app('" . BladeRouteGenerator::class . "')->generate({$group}{$guard}); ?>";
         });
 
         if ($this->app->runningInConsole()) {
